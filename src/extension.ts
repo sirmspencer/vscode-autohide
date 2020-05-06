ce74569c90264060864c52229c4877f6975e9fa2
Matt Spencer <sirmspencer@gmail.com>
2020-05-06 10:30:23 -0700

    Move getConfiguration inside the event to get most recent settings

--- src/extension.ts
+++ src/extension.ts
@@ -3,32 +3,30 @@ import * as vscode from "vscode";
 import { TextEditorSelectionChangeKind } from "vscode"
 
 export function activate(context: vscode.ExtensionContext) {
-	let config = vscode.workspace.getConfiguration("autoHide");
-	if (true) {
-		vscode.window.onDidChangeTextEditorSelection(selection => {
-			let path = vscode.window.activeTextEditor.document.fileName;
-			let pathIsFile = path.includes(".") || path.includes("\\") || path.includes("/");
+	vscode.window.onDidChangeTextEditorSelection(selection => {
+		let config = vscode.workspace.getConfiguration("autoHide");
+		let path = vscode.window.activeTextEditor.document.fileName;
+		let pathIsFile = path.includes(".") || path.includes("\\") || path.includes("/");
 
-			if (selection.kind != TextEditorSelectionChangeKind.Mouse // selection was not from a click
-				|| selection.selections.length != 1                   // no selections or multiselections
-				|| selection.selections.find(a=>a.isEmpty) == null    // multiselections
-				|| !pathIsFile) {                                     // The debug window editor
-				return;
-			} else {
-				setTimeout(function() {
-					if (config.autoHidePanel) {
-						vscode.commands.executeCommand("workbench.action.closePanel");
-					}
-				}, config.panelDelay);
+		if (selection.kind != TextEditorSelectionChangeKind.Mouse // selection was not from a click
+			|| selection.selections.length != 1                   // no selections or multiselections
+			|| selection.selections.find(a=>a.isEmpty) == null    // multiselections
+			|| !pathIsFile) {                                     // The debug window editor
+			return;
+		} else {
+			setTimeout(function() {
+				if (config.autoHidePanel) {
+					vscode.commands.executeCommand("workbench.action.closePanel");
+				}
+			}, config.panelDelay);
 
-				setTimeout(function() {
-					if (config.autoHideSideBar) {
-						vscode.commands.executeCommand("workbench.action.closeSidebar");
-					}
-				}, config.sideBarDelay);
-			};
-		});
-	}
+			setTimeout(function() {
+				if (config.autoHideSideBar) {
+					vscode.commands.executeCommand("workbench.action.closeSidebar");
+				}
+			}, config.sideBarDelay);
+		};
+	});
 }
 
 export function deactivate() {}
